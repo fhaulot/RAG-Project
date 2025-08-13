3ec47928a04126266370380e13193b3d55826d96
fhaulot <fhaulot@gmail.com>
2025-08-13 16:37:47 +0200

    adding oop ideas structures

---
+++ OOP.py
@@ -0,0 +1,8 @@
+""" In this file, we'll get classes about RAG
+1. loading and getting the environment variables
+2. using the api key
+3. creating context
+4. asking for prompt
+5. getting response
+6. getting embeddings (prompts and chunks)
+"""
